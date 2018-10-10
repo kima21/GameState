458a5e1ed1148a34b20f3ff09c669c270128150b
ashley <kima21@up.edu>
2018-10-09 20:31:41 -0700

    a

--- app/src/main/java/com/example/ashley/gamestate/GameState.java
+++ app/src/main/java/com/example/ashley/gamestate/GameState.java
@@ -1,7 +1,12 @@
 package com.example.ashley.gamestate;
 
 import java.util.ArrayList;
-
+/**
+ * Game State Class for Uno
+ *
+ * @author Andrew, Meredith, Ashley
+ * @date October 2018
+ */
 public class GameState {
     int playerId;
     int deckSize;
@@ -11,4 +16,20 @@ public class GameState {
     ArrayList<Card> hand1 = new ArrayList<Card>(7);
     ArrayList<Card> hand2 = new ArrayList<Card>(7);
     Card discardPile;
+
+
+
+    public String getGameState(){
+        return "Player ID: " + playerId + ", Deck Size: " + deckSize + ", Turn: " + turn;
+    }
+
+    /**toString
+     *
+     * @return Game State
+     */
+    @Override
+    public String toString(){
+        return "[" +getGameState()+ "]";
+    }
+
 }
