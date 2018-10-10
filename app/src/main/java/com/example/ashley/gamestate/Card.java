f20ea653b199d4f256b44460c0a6e60164ba7b4d
ashley <kima21@up.edu>
2018-10-09 19:26:58 -0700

    started initializing

--- app/src/main/java/com/example/ashley/gamestate/Card.java
+++ app/src/main/java/com/example/ashley/gamestate/Card.java
@@ -1,6 +1,15 @@
 package com.example.ashley.gamestate;
 
-public class Card {
-    
+import android.graphics.Color;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+
+public class Card {
+    int[] allCard;
+
+//Color to int transition
+    int red = Color.RED;
+    
 }
--- app/src/main/java/com/example/ashley/gamestate/GameState.java
+++ app/src/main/java/com/example/ashley/gamestate/GameState.java
@@ -1,5 +1,10 @@
 package com.example.ashley.gamestate;
 
 public class GameState {
+    int playerId;
+    int deckSize;
+    int turn;
+
+
 
 }
