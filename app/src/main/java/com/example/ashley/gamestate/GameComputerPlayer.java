8cdde2ea0325598f06e5449487161d00b506507c
ashley <kima21@up.edu>
2018-10-09 20:28:44 -0700

    a

---
+++ app/src/main/java/com/example/ashley/gamestate/GameComputerPlayer.java
@@ -0,0 +1,10 @@
+package com.example.ashley.gamestate;
+
+/**
+ * Computer A.I. for Uno
+ *
+ * @author Andrew, Meredith, Ashley
+ * @date October 2018
+ */
+public class GameComputerPlayer {
+}
--- app/src/main/java/com/example/ashley/gamestate/GameState.java
+++ app/src/main/java/com/example/ashley/gamestate/GameState.java
@@ -1,10 +1,35 @@
 package com.example.ashley.gamestate;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+
+/**
+ * Game State Class for Uno
+ *
+ * @author Andrew, Meredith, Ashley
+ * @date October 2018
+ */
 public class GameState {
     int playerId;
     int deckSize;
     int turn;
 
+    public GameState(){
 
+    }
+
+    public String getGameState(){
+        return "Player ID: " + playerId + ", Deck Size: " + deckSize + ", Turn: " + turn;
+    }
+
+    /**toString
+     *
+     * @return Game State
+     */
+    @Override
+    public String toString(){
+       return "[" +getGameState()+ "]";
+    }
 
 }
