16cb5aee6ac1944bba47d7f6978e72e1cfa569f1
tagawa21 <43159718+tagawa21@users.noreply.github.com>
2018-10-09 21:01:17 -0700

    GameState constructor (incomplete) and makeDeck

--- app/src/main/java/com/example/ashley/gamestate/GameState.java
+++ app/src/main/java/com/example/ashley/gamestate/GameState.java
@@ -12,10 +12,106 @@ public class GameState {
     int deckSize;
     int turn;
     Card deck[] = new Card[108];
-    int idArr[] = new int[108];
+    String idArr[] = new String[108];
     ArrayList<Card> hand1 = new ArrayList<Card>(7);
     ArrayList<Card> hand2 = new ArrayList<Card>(7);
     Card discardPile;
+
+    public GameState(){
+        playerId = 0;
+        deckSize = deck.length;
+        turn = 0;
+        makeDeck();
+
+    }
+
+    public void makeDeck(){
+        int i, n, k, j;
+
+        //number cards with value 0
+        for(i = 0; i < 4; i++){
+            deck[i].setColor(i);
+            deck[i].setValue(0);
+            deck[i].setType('n');
+            idArr[i] = "card"+i;
+        }
+
+        //number cards with values 1-9
+        while(i < 76){
+            for(n = 0; n < 2; n++){
+                for(k = 0; k < 10; k++){
+                    for(j = 0; j < 4; j++){
+                        deck[i].setColor(j);
+                        deck[i].setValue(k);
+                        deck[i].setType('n');
+                        idArr[i] = "card"+i;
+                        i++;
+                    }
+                }
+            }
+        }
+
+        //skip cards
+        while(i < 84){
+            for(k = 0; k < 2; k++){
+                for(j = 0; j < 4; j++){
+                    deck[i].setColor(j);
+                    deck[i].setValue(0);
+                    deck[i].setType('s');
+                    idArr[i] = "card"+i;
+                    i++;
+                }
+            }
+        }
+
+        //draw 2 cards
+        while(i < 92){
+            for(k = 0; k < 2; k++){
+                for(j = 0; j < 4; j++){
+                    deck[i].setColor(j);
+                    deck[i].setValue(0);
+                    deck[i].setType('d');
+                    idArr[i] = "card"+i;
+                    i++;
+                }
+            }
+        }
+
+        //reverse cards
+        while(i < 100){
+            for(k = 0; k < 2; k++){
+                for(j = 0; j < 4; j++){
+                    deck[i].setColor(j);
+                    deck[i].setValue(0);
+                    deck[i].setType('r');
+                    idArr[i] = "card"+i;
+                    i++;
+                }
+            }
+        }
+
+        //wild cards
+        while(i < 104){
+            for(j = 0; j < 4; j++){
+                deck[i].setColor(4);
+                deck[i].setValue(0);
+                deck[i].setType('w');
+                idArr[i] = "card"+i;
+                i++;
+            }
+        }
+
+        //wild draw 4 cards
+        while(i < 108){
+            for(j = 0; j < 4; j++){
+                deck[i].setColor(4);
+                deck[i].setValue(0);
+                deck[i].setType('d');
+                idArr[i] = "card"+i;
+                i++;
+            }
+        }
+    }
 
     public String getGameState(){
         return "Player ID: " + playerId + ", Deck Size: " + deckSize + ", Turn: " + turn;
