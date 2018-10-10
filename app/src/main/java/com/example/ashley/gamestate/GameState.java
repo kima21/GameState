1d60de026593d9f24cb17dd0ee4068540233fc94
tagawa21 <43159718+tagawa21@users.noreply.github.com>
2018-10-09 19:55:06 -0700

    GameState instance variables commit

--- app/src/main/java/com/example/ashley/gamestate/GameState.java
+++ app/src/main/java/com/example/ashley/gamestate/GameState.java
@@ -1,10 +1,15 @@
 package com.example.ashley.gamestate;
 
+import java.util.ArrayList;
+
 public class GameState {
     int playerId;
     int deckSize;
     int turn;
-
-
+    Card deck[] = new Card[108];
+    int idArr[] = new int[108];
+    ArrayList<Card> hand1 = new ArrayList<Card>(7);
+    ArrayList<Card> hand2 = new ArrayList<Card>(7);
+    Card discardPile;
 
 }
